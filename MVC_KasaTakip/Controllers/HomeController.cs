35a272fdcd8bb8c34db98ed6daaa69b27f635f55
Furkan Boztepe <furkanboztepe58@hotmail.com>
2021-10-21 19:47:51 +0300

    proje tamamlandı.

---
+++ MVC_KasaTakip/Controllers/HomeController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_KasaTakip.Models.Entity;
+
+namespace MVC_KasaTakip.Controllers
+{
+
+   
+    public class HomeController : Controller
+    {
+       
+        kasataki_turgayEntities db = new kasataki_turgayEntities();
+       
+        public ActionResult Index(string ara)
+        {
+
+            kasataki_turgayEntities entities = new kasataki_turgayEntities();
+
+            var model = db.Islemler.ToList();
+            if (!string.IsNullOrEmpty(ara))
+            {
+                model = model.Where(x => x.islem_tur.Contains(ara)).ToList();
+                return View(model);
+            }
+            else
+            {
+                return View(from Islemler in entities.Islemler.Take(10)
+                            select Islemler);
+            }
+            //Burada eğer kullanıcı bir arama yapmışsa aramaya uyan verileri yapmamışsa tüm verileri tablo halinde hizmetine sunuyoruz.
+        }
+        [HttpGet]
+        public ActionResult About()
+        {
+
+
+            return View();
+        }
+        //About sayfası action controllerimiz burada extra bir işlem bulunmamakta.
+
+        public ActionResult Ekle()
+        {
+            return View();
+        }
+        public ActionResult Ekle2(Islemler p)
+        {
+            db.Islemler.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        //Burada İslemler tablosuna ekleyi kaydedip index sayfasına dönüyoruz.
+        public ActionResult Guncellebilgilerigetir(Islemler p)
+        {
+            var model = db.Islemler.Find(p.islem_no);
+            
+            return View();
+
+        }
+        public ActionResult Guncelle(Islemler p)
+        {
+            db.Entry(p).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        //Burada ise istenen islemi güncelleyip güncellenmiş halini index sayfasına sunuyoruz.
+
+        public ActionResult silbilgigetir(Islemler p)
+        {
+            var model = db.Islemler.Find(p.islem_no);
+            if (model == null) return HttpNotFound();
+            return View(model);
+        }
+        public ActionResult Sil(Islemler p)
+        {
+            db.Entry(p).State = System.Data.Entity.EntityState.Deleted;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        //Burada ise istenen islemi silerek tablonun güncel halini müşteriye sunuyoruz.
+
+
+
+    }
+}
---
+++ MVC_KasaTakip/Controllers/KullanicilarController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_KasaTakip.Models.Entity;
+using System.Web.Security;
+
+namespace MVC_KasaTakip.Controllers
+{
+  [AllowAnonymous]
+    public class KullanicilarController : Controller
+    {
+        // GET: Kullanicilar
+
+        kasataki_turgayEntities db = new kasataki_turgayEntities();
+        [HttpGet]
+
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Kullanicilar k)
+        {
+            
+            var kullanıcı = db.Kullanicilar.FirstOrDefault(x=> x.KullaniciAdi==k.KullaniciAdi && x.Sifre==k.Sifre);
+            if (kullanıcı != null)
+            {
+
+                FormsAuthentication.SetAuthCookie(k.KullaniciAdi, false);
+                return RedirectToAction("Index","Home");
+            }
+            ViewBag.Hata = "Kullanıcı Adı veya Şifre Yanlış";
+            return View();
+        }
+        //Burada siteden girilen verileri veritabanında ki veriler ile kıyaslıyor ve kullanıcı adımız boş veya şifremiz yanlış değilse index sayfasına taşıyoruz.
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+        //Burası da Kullanıcımızın site ile işi bittiğinde çıkış yapmasını sağlayan controllerimiz.
+        [HttpGet]
+        public ActionResult Kaydol()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Kaydol(Kullanicilar k)
+        {
+            if (!ModelState.IsValid) return View();
+
+            db.Entry(k).State = System.Data.Entity.EntityState.Added;
+            db.SaveChanges();
+            return RedirectToAction("Login");
+        }
+        //Burada ise kayıt olmak isteyen kullanıcımız için verileri önce ekleyip sonra kayıt ediyoruz.
+    }
+}
